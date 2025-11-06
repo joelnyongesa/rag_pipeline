ca51e6b8729cef977cfc73e6bd8b34df62b74906
Joel Nyongesa <joelnyongesa148@gmail.com>
2025-11-06 11:02:24 +0300

    feat: included module docstring and final newline

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+This module serves as the entry point for the FastAPI application.
+
+It initializes the FastAPI instance and includes routers for different API endpoints (eg. query).
+"""
+
 from fastapi import FastAPI
 from src.routes.ingest_data import ingest_router
 
